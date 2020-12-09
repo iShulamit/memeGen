f92ab34f00909a97bfb05f388b74d08502ee94e3
iShulamit <shidji@gmail.com>
2020-12-09 20:17:59 +0200

    fixing files
    replacing files

---
+++ Day29-Sprint2/Sprint2-Delivery/Sprint2-Delivery/Delivery1 - Wednesday 2000/js/storage-service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('storage services js is connected');
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val));
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key);
+    return JSON.parse(val);
+}
